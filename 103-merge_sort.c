0be816cd3650bbcaa8cf661a9c3307f0f26063ce
muluante <muluante@outlook.com>
2023-01-24 06:53:18 -0800

    103-merge_sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,99 +1,110 @@
 #include "sort.h"
-
+#include <stdio.h>
 /**
- * int_array_copy - copies an int array of size size
- *
- * @array: the array to copy
- * @size: the wanted size of the copied array
- *
- * Return: the new copied array, or NULL on failure
+ *_calloc - this is a calloc function
+ *@nmemb: number of elemets
+ *@size: bit size of each element
+ *Return: pointer to memory assignement
  */
-
-int *int_array_copy(int *array, size_t size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *new = NULL;
-	size_t i;
+	unsigned int i = 0;
+	char *p;
 
-	new = malloc(sizeof(int) * size);
-	if (new)
-	{
-		for (i = 0; i < size; i++)
-			new[i] = array[i];
-	}
-
-	return (new);
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	p = malloc(nmemb * size);
+	if (p == '\0')
+		return ('\0');
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
 }
-
 /**
- * top_down_merge - merges the two arrays
- *
- * @a: the first array of integers
- * @b: the second array of integers
- * @size: the size of the final array
- * @mid: the middle index of the array
- */
-
-void top_down_merge(int *a, int *b, size_t size, size_t mid)
+ *merge - make a merge
+ *@arr: one from start to mid
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: first element position
+ *@mid: array middle
+ *@end: last element position
+ **/
+void merge(int *arr, int *tmp, int start, int mid, int end)
 {
-	size_t i = 0, j = 0, k;
+	/*  sizes and temp arrays */
+	int size_left = mid - start + 1, size_right = end - mid;
+	int *array_left, *array_right;
+	/* counters */
+	int left, right, i = 0;
 
-	for (k = 0; k < size; k++)
+	array_left = &tmp[0];
+	array_right = &tmp[size_right];
+	for (left = 0; left < size_left; left++)
+		array_left[left] = arr[start + left];
+	for (right = 0; right < size_right; right++)
+		array_right[right] = arr[mid + 1 + right];
+	left = 0, right = 0, i = start;
+	/* merging tmp arrays into main array*/
+	while (left < size_left && right < size_right)
 	{
-		if (j >= size - mid || (i < mid && a[i] < (a + mid)[j]))
-			b[k] = a[i++];
+		if (array_left[left] <= array_right[right])
+			arr[i] = array_left[left], left++;
 		else
-			b[k] = (a + mid)[j++];
+			arr[i] = array_right[right], right++;
+		i++;
 	}
-
-	for (k = 0; k < size; k++)
-		a[k] = b[k];
-}
-
-/**
- * top_down_split_merge - merges the two arrays
- *
- * @a: the first array of integers
- * @b: the second array of integers
- * @size: the size of the final array
- */
-
-void top_down_split_merge(int *a, int *b, size_t size)
-{
-	size_t mid = size / 2 /*, i = 0, j = 0, k*/;
-
-	if (size < 2)
-		return;
-
-	top_down_split_merge(a, b, mid);
-	top_down_split_merge(a + mid, b + mid, size - mid);
-
+	/* merging remaining left array into main array*/
+	while (left < size_left)
+		arr[i] = array_left[left], left++, i++;
+	/* merging remaining right array into main array*/
+	while (right < size_right)
+		arr[i] = array_right[right], right++, i++;
 	printf("Merging...\n");
 	printf("[left]: ");
-	print_array(a, mid);
+	print_array(array_left, left);
 	printf("[right]: ");
-	print_array(b + mid, size - mid);
-
-	top_down_merge(a, b, size, mid);
-
+	print_array(array_right, right);
 	printf("[Done]: ");
-	print_array(a, size);
+	print_array(&arr[start], left + right);
 }
-
 /**
- * merge_sort - sorts an array of integers using merge sort
- *
- * @array: the array of integers
- * @size: the size of the array
+ *mergesort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@array: array of integers
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: fisrt element position
+ *@end: last element position
+ *Return: void
  */
+void mergesort(int *array, int *tmp, int start, int end)
+{
+	int mid;
 
+	mid = (start + end) / 2;
+	if ((start + end) % 2 == 0)
+		mid--;
+	if (mid >= start)
+	{
+		mergesort(array, tmp, start, mid);
+		mergesort(array, tmp, mid + 1, end);
+		merge(array, tmp, start, mid, end);
+	}
+}
+/**
+ *merge_sort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@size: size of the list
+ *@array: array of integers
+ *Return: void
+ */
 void merge_sort(int *array, size_t size)
 {
-	int *work = NULL;
+	int *tmp;
 
-	work = int_array_copy(array, size);
-	if (work)
-	{
-		top_down_split_merge(array, work, size);
-		free(work);
-	}
+	if (!array || size < 2)
+		return;
+	tmp = _calloc(size, sizeof(int));
+	mergesort(array, tmp, 0, size - 1);
+	free(tmp);
 }
