15268e748adde92ee27773b23cc5f0e1dcbfd581
muluante <muluante@outlook.com>
2023-01-25 03:20:15 -0800

    103-merge_sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,78 +1,84 @@
 #include "sort.h"
 
 /**
- * merge_sort-Write a function tha print order list
- * @array:un order list
- * @size:size of unorder list
- */
+* merge - Merges the splits from merge_sorty
+* @array: Array split to merge
+* @low: lowest index of split
+* @middle: middle index of split
+* @high: high index of split
+* @temp: temp array for merging
+*/
 
+void merge(int *array, int low, int middle, int high, int *temp)
+{
+	int i, j, k, l = 0, r = 0, n, left[4096], right[4096];
+
+	printf("Merging...\n");
+	i = low, j = middle + 1, k = l = 0;
+	while (i <= middle && j <= high)
+	{
+		if (array[i] <= array[j])
+			temp[k] = left[l] = array[i], k++, i++, l++;
+		else
+			temp[k] = right[r] = array[j], k++, j++, r++;
+	}
+	while (i <= middle)
+		temp[k] = left[l] = array[i], k++, i++, l++;
+	while (j <= high)
+		temp[k] = right[r] = array[j], k++, j++, r++;
+	printf("[left]: ");
+	for (n = 0; n < l; n++)
+		(n == 0) ? printf("%d", left[n]) : printf(", %d", left[n]);
+	printf("\n[right]: ");
+	for (n = 0; n < r; n++)
+		(n == 0) ? printf("%d", right[n]) : printf(", %d", right[n]);
+	printf("\n[Done]: ");
+	for (i = low; i <= high; i++)
+	{
+		array[i] = temp[i - low], printf("%d", array[i]);
+		if (i != high)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+
+/**
+* merge_sorty - recurrsive function utilizing merge sort algo
+* @array: Array
+* @low: Lowest index of split
+* @high: highest index of split
+* @temp: temp array for mergin
+*/
+
+void merge_sorty(int *array, int low, int high, int *temp)
+{
+	int middle;
+
+	if (low < high)
+	{
+		middle = ((high + low - 1) / 2);
+		merge_sorty(array, low, middle, temp);
+		merge_sorty(array, middle + 1, high, temp);
+		merge(array, low, middle, high, temp);
+	}
+}
+
+/**
+* merge_sort - Sorts array with merge sort algo
+* @array: array to sort
+* @size: Size of array to sort
+*/
 
 void merge_sort(int *array, size_t size)
 {
-	int *new_arr;
+	int *temp;
 
-	new_arr = malloc(sizeof(int) * size);
-
-	merge_recursion(array, new_arr, 0, size);
-	free(new_arr);
-}
-
-/**
- * merge_recursion-recursion of list
- * @array:list
- * @new_arr:new list
- * @start:list to start
- * @end:end of list
- */
-
-void merge_recursion(int *array, int *new_arr, size_t start, size_t end)
-{
-	size_t mid;
-
-	if ((end - start) > 1)
-	{
-		mid = (end - start) / 2 + start;
-		merge_recursion(array, new_arr, start, mid);
-		merge_recursion(array, new_arr, mid, end);
-		merge_re_sort(array, new_arr, start, mid, end);
-	}
-}
-
-/**
- * merge_re_sort-sort list by using divid and qu
- * @array:list of orginal array
- * @new_arr:new_orderd array
- * @start:list is start from 0
- * @end:list end in size
- * @mid:mid of list
- */
-
-void merge_re_sort(int *array, int *new_arr, size_t start,
-		size_t mid, size_t end)
-{
-	size_t i, j, k = 0;
-
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(array + start, mid - start);
-	printf("[right]: ");
-	print_array(array + mid, end - mid);
-	for (i = start, j = mid; i < mid && j < end; k++)
-	{
-		if (array[i] < array[j])
-			new_arr[k] = array[i++];
-		else
-			new_arr[k] = array[j++];
-	}
-	while (i < mid)
-		new_arr[k++] = array[i++];
-	while (j < end)
-		new_arr[k++] = array[j++];
-	for (k = start, i = 0; k < end; k++)
-	{
-		array[k] = new_arr[i++];
-	}
-
-	printf("[Done]: ");
-	print_array(array + start, end - start);
+	if (array == NULL || size < 2)
+		return;
+	temp = malloc(sizeof(int) * (size + 1));
+	if (temp == NULL)
+		return;
+	merge_sorty(array, 0, size - 1, temp);
+	free(temp);
 }
